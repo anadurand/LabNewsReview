049ca219ad8ad244d65e0a21593b76cab2c64e47
FiorellaQA <fiorella_90_17@hotmail.com>
2017-07-10 18:07:38 -0500

    añadiendo estilo

--- src/assets/js/components/navMenu.js
+++ src/assets/js/components/navMenu.js
@@ -2,7 +2,7 @@
 const NavBAr = () => {
   const parent = $('<nav class="navbar menu"></nav>');
   const div =$('<div class="container-fluid"></div>');
-  const divHeader = $('<div class="navbar-header"><button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" arial-expanded="false"><img src="assets/img/menu.png"></button><a href="#" class="hidden-md hidden-lg"><img src="assets/img/logoicon.png" class=""></a></div>')
+  const divHeader = $('<div class="navbar-header"><button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu" arial-expanded="false"><img src="assets/img/menu.png"></button><a href="#" class="hidden-md hidden-lg"><img class="logo_mobile" src="assets/img/logoicon.png" class=""></a></div>')
   const menu = $('<div class="collapse navbar-collapse" id = "menu"><ul class="nav navbar-nav lista"><li>Lo último</li><li>Opinión</li><li>Cultura</li><li>Perú</li><li>Tecnología</li><li>Mundo</li><li>Economía</li><li>Lifestyle</li><li>Deporte</li></ul></div>');
 
 
